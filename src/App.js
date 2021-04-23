1d7e8aa8b05c7ed374f2fb95ff14faf408fbf2f2
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-04-22 21:53:11 -0700

    setup styling and nav

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
+// images
+import topFiveLogo from "./img/top-five-logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+// components
+import Table from "./components/Table";
+import SideBar from "./components/SideBar";
+// styles
+import "./styles/app.scss";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout-grid">
+      <SideBar />
+      <div className="content">
+        <div className="header">
+          <img src={topFiveLogo} alt="logo" />
+          <div className="search-bar">
+            <FontAwesomeIcon className="search-icon" icon={faSearch} />
+            <input type="text" placeholder="Search for a league" />
+          </div>
+        </div>
+        <h1>English Premier League</h1>
+      </div>
     </div>
   );
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="side-bar">
+      <ul>
+        <li className="selected">Home</li>
+        <li>Table</li>
+        <li>Matches</li>
+        <li>Stats</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
