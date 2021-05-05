8db394735bbd74be45fe913fe6421972d98863e5
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-05-04 21:11:30 -0700

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -4,41 +4,40 @@ import topFiveLogo from "./img/top-five-logo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 // components
-import Table from "./components/Table";
 import SideBar from "./components/SideBar";
 // styles
 import "./styles/app.scss";
-import { ThemeConsumer } from "styled-components";
 
 function App() {
   // variables
   let allLeaguesList = [];
-  let count = 0;
   let randomNumber;
 
   //useState
   const [league, setLeague] = useState([]);
 
   // useEffect
+  /*
   useEffect(() => {
     fetch("https://www.thesportsdb.com/api/v1/json/1/all_leagues.php")
       .then((resp) => resp.json())
       .then((data) => {
         for (let i = 0; i < data.leagues.length; i++) {
-          if (data.leagues[i].strSport == "Soccer") {
+          if (data.leagues[i].strSport === "Soccer") {
             allLeaguesList.push(data.leagues[i].strLeague);
           }
         }
         setLeague(allLeaguesList[0]);
       });
   }, []);
+    */
 
   useEffect(() => {
     fetch("https://www.thesportsdb.com/api/v1/json/1/all_leagues.php")
       .then((resp) => resp.json())
       .then((data) => {
         for (let i = 0; i < data.leagues.length; i++) {
-          if (data.leagues[i].strSport == "Soccer") {
+          if (data.leagues[i].strSport === "Soccer") {
             allLeaguesList.push(data.leagues[i].strLeague);
           }
         }
@@ -49,9 +48,6 @@ function App() {
   const getRandomLeague = () => {
     randomNumber = Math.floor(Math.random(allLeaguesList.length) * 100);
     setLeague(allLeaguesList[randomNumber]);
-    console.log(allLeaguesList[randomNumber]);
-    console.log(allLeaguesList);
-    console.log(randomNumber);
   };
 
   return (
