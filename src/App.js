55b4bd888757ce36987f7f072ebc500d5a54eae6
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-05-10 19:57:19 -0700

    added input suggestions and styles

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ function App() {
   // variables
   let randomNumber;
   const url = "https://www.thesportsdb.com/api/v1/json/1/all_leagues.php";
+  const AUTOCOMPLETE_OPTIONS = 4;
+  let suggestions = [];
 
   //useState
   const [league, setLeague] = useState(null);
   const [leagueList, setLeagueList] = useState(null);
   const [leagueId, setLeagueId] = useState(0);
   const [leagueIdList, setLeagueIdList] = useState(null);
+  const [suggestedLeagues, setSuggestedLeagues] = useState([]);
 
   useEffect(() => {
     let listOfLeagues = [];
@@ -43,10 +46,31 @@ function App() {
     randomNumber = Math.floor(Math.random(leagueList.length) * 100);
     setLeague(leagueList[randomNumber]);
     setLeagueId(leagueIdList[randomNumber]);
+    setSuggestedLeagues([]);
   };
 
-  const keyup = () => {
-    console.log("hello");
+  const loadSuggestions = (event) => {
+    const userInput = capitalizeInput(event.target.value);
+    suggestions = leagueList.filter((league) => {
+      return league.includes(userInput);
+    });
+    if (suggestions.length > AUTOCOMPLETE_OPTIONS) {
+      suggestions.length = AUTOCOMPLETE_OPTIONS;
+    }
+    setSuggestedLeagues(suggestions);
+  };
+
+  const capitalizeInput = (userInput) => {
+    if (!userInput) {
+      return;
+    }
+    const words = userInput.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      if (words[i]) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+      }
+    }
+    return words.join(" ");
   };
 
   return (
@@ -58,12 +82,20 @@ function App() {
           <div className="search-bar">
             <FontAwesomeIcon className="search-icon" icon={faSearch} />
             <input
-              onKeyUp={keyup}
+              onKeyUp={(event) => loadSuggestions(event)}
               type="text"
               placeholder="Search for a league..."
             />
           </div>
           <button onClick={getRandomLeague}>Random League</button>
+          <div className="suggestion-container">
+            {suggestedLeagues.map((league) => (
+              <div>
+                <p key={league}>{league}</p>
+                <hr />
+              </div>
+            ))}
+          </div>
         </div>
         <h1>{league}</h1>
         <p>The League ID is {leagueId}</p>
